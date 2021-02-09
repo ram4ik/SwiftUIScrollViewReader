fe032e51081867e546087145e615551c2a6c7367
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-02-09 14:34:55 +0200

    Initial Commit

---
+++ SwiftUIScrollViewReader/SwiftUIScrollViewReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIScrollViewReaderApp.swift
+//  SwiftUIScrollViewReader
+//
+//  Created by ramil on 09.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIScrollViewReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
